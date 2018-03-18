2bbdb61f6d8cecfbdcbab5456cdc545c20331cf6
César Pinto Castillo <cesar@jagcesar.se>
2018-03-18 09:10:12 -0700

    Fix so CI only builds (#29)
    * Adds empty credentials file so tests compile
    
    * Creates EmptyTests target
    
    The empty tests are now run on the iOS scheme. Let's keep it like
    this until we have mocked objects for the tests.
    
    The purpose of doing this is to make sure our CI passes the builds.

---
+++ Tests/Credentials.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import TeslaAPI
+
+extension TeslaAPITests {
+    func username() -> String {
+        return ""
+    }
+    func password() -> String {
+        return ""
+    }
+    func accessToken() -> String {
+        return ""
+    }
+    func vehicleIdentifier() -> String {
+        return ""
+    }
+}
